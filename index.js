d57aac79bd0ac7e8a03a8f7fe13b91b87318e40e
Shane Lucy <shanelucy01@protonmail.com>
2021-10-15 18:49:58 +0100

    Formatting changes

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  extends: ["typescript-airbnb-prettier", "plugin:@ota-meshi/svelte/recommended"],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "typescript-airbnb-prettier",
+    "plugin:@ota-meshi/svelte/recommended",
+  ],
   plugins: ["svelte3"],
-  ignorePatterns: ['*.cjs, **/*.js'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  ignorePatterns: ["*.cjs, **/*.js"],
+  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
   settings: {
-    'svelte3/typescript': () => require('typescript')
+    "svelte3/typescript": () => require("typescript"),
   },
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module",
     ecmaVersion: 12,
-    project: './tsconfig.json'
+    project: "./tsconfig.json",
   },
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
+    "import/no-extraneous-dependencies": [
+      "error",
       {
-        devDependencies: true
-      }
-    ]
-  }
-}
+        devDependencies: true,
+      },
+    ],
+  },
+};
